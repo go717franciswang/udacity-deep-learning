5e1ce12840a0968cb9419e1ebc79770d971ff56a
Francis Wang <go717franciswang@gmail.com>
2016-02-10 17:47:09 -0800

    Init

---
+++ docker2host.sh
@@ -0,0 +1,6 @@
+docker cp udacity:/notebooks/1_notmnist.ipynb ./
+docker cp udacity:/notebooks/2_fullyconnected.ipynb ./
+docker cp udacity:/notebooks/3_regularization.ipynb ./
+docker cp udacity:/notebooks/4_convolutions.ipynb ./
+docker cp udacity:/notebooks/5_word2vec.ipynb ./
+docker cp udacity:/notebooks/6_lstm.ipynb ./
